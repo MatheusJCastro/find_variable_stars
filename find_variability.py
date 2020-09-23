7c8378bed8aece56ac16c5f63bb984f0262a86ca
MatheusJCastro <matheusdejesuscastro@gmail.com>
2020-09-23 13:38:30 -0300

    Add IQR and 1/ETA weighted methods

--- find_variability.py
+++ find_variability.py
@@ -80,37 +80,48 @@ def plot_data(data, show=False):
     plt.close()
 
 
-def save_results(iqr, eta, dir_name, fl_name):
+def save_results(iqr, iqr_weight, eta, eta_weight, dir_name, fl_name):
     if not os.path.isdir("results_" + dir_name):
         os.mkdir("results_" + dir_name)
     fl_name = "results_" + fl_name
 
     iqr = np.array(list(iqr.items()))
+    iqr_weight = np.array(list(iqr_weight.items()))
     eta = np.array(list(eta.items()))
+    eta_weight = np.array(list(eta_weight.items()))
 
     save = "Method, Filter, Result\n"
 
     for i in range(len(iqr)):
         save += "IQR, {}, {}\n".format(iqr[i][0], iqr[i][1])
+    for i in range(len(iqr_weight)):
+        save += "IQR Weighted, {}, {}\n".format(iqr_weight[i][0], iqr_weight[i][1])
     for i in range(len(eta)):
         save += "ETA, {}, {}\n".format(eta[i][0], eta[i][1])
+    for i in range(len(iqr_weight)):
+        save += "ETA Weighted, {}, {}\n".format(eta_weight[i][0], eta_weight[i][1])
 
     np.savetxt(fl_name, [save], fmt="%s")
 
 
-def iqr_method(data):
+def iqr_method(data, weight=None):
     # Find the IQR value for each filter of an file opened
+    if weight is None:
+        weight = {}
+        for i in data.keys():  # create a sequence of lists with ones (null element) if any weight is given
+            weight[i] = np.ones(len(data[i][1]))
+
     iqr = {}
     for i in data.keys():
         data_len = len(data[i][1])
         if data_len != 1:
-            data_of_interest = np.sort(data[i][1])[data_len//4:-data_len//4]  # Exclude the 25% lower and highest values
+            data_of_interest = np.sort(data[i][1]*weight[i])[data_len//4:-data_len//4]  # Exclude the 25% lower and highest values
         else:
             data_of_interest = data[i][1]
 
         data_len = len(data_of_interest)
         if data_len != 1:
-            # Diference between the median of the upper and lower halves
+            # Difference between the median of the upper and lower halves
             iqr[i] = np.median(data_of_interest[data_len//2:]) - np.median(data_of_interest[:data_len//2])
         else:
             iqr[i] = 0
@@ -118,29 +129,61 @@ def iqr_method(data):
     return iqr
 
 
-def eta_method(data):
+def eta_method(data, weight=None):
     # Find the IQR value for each filter of an file opened
+    if weight is None:
+        weight = {}
+        for i in data.keys():  # create a sequence of lists with ones (null element) if any weight is given
+            weight[i] = np.ones(len(data[i][1]))
+
     eta = {}
     for i in data.keys():
         data_len = len(data[i][1])-1
 
         mean_square = 0
         for j in range(data_len):  # find mean square successive difference
-            mean_square += (data[i][1][j+1] - data[i][1][j]) ** 2 / data_len
+            mean_square += (data[i][1][j+1]*weight[i][j+1] - data[i][1][j]*weight[i][j]) ** 2 / data_len
 
-        if data_len != 0:
-            var = 0
-            for j in range(data_len+1):  # find variance manually
-                var += (data[i][1][j] - np.mean(data[i][1]))**2 / data_len
+        # if data_len != 0:
+        #    var = 0
+        #    for j in range(data_len+1):  # find variance manually
+        #        var += (data[i][1][j] - np.mean(data[i][1]))**2 / data_len
 
-        var = np.std(data[i][1])**2  # find the variance
+        var = np.std(data[i][1]*weight[i])**2  # find the variance
 
         if var != 0:
-            eta[i] = mean_square/var
+            eta[i] = var/mean_square
         else:
             eta[i] = 0
 
     return eta
+
+
+def error_weights(data):
+    # Create a sequence of lists using the data magnitude error
+    weights = {}
+    for i in data.keys():
+        weights[i] = np.array(data[i][2])
+
+    return weights
+
+
+def time_weights(data, threshold=5):  # threshold defined in minutes
+    # Find time weights for the data based on when the image was taken
+    threshold = threshold/1440  # transform to day
+
+    weights = {}
+    for i in data.keys():
+        filter_weights = [1]
+        for j in range(len(data[i][0])-1):
+            diff = data[i][0][j+1] - data[i][0][j]
+            if diff <= threshold:
+                filter_weights.append(1)
+            else:
+                filter_weights.append(1/diff)
+        weights[i] = np.array(filter_weights)
+
+    return weights
 
 
 def main(dir_name):
@@ -152,9 +195,11 @@ def main(dir_name):
         data = format_data(data)
 
         iqr_results = iqr_method(data)
+        iqr_results_weighted = iqr_method(data, weight=error_weights(data))
         eta_results = eta_method(data)
+        eta_results_weighted = eta_method(data, weight=time_weights(data))
 
-        save_results(iqr_results, eta_results, dir_name, fl_name)
+        save_results(iqr_results, iqr_results_weighted, eta_results, eta_results_weighted, dir_name, fl_name)
         # plot_data(data)
 
 
